08b21078e4a992ddd774cd7ac3b5d8649c7522ca
idrgn <37042855+idrgn@users.noreply.github.com>
2023-05-15 09:56:43 +0200

    Movido Header a index y ajuste de color

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -2,17 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import { MainPage } from "./screens/main_page/MainPage";
 import { NotFound } from "./screens/not_found/NotFound";
-import { Header } from "./components/header/Header";
 
 import "./App.css";
 
 function App() {
 	return (
 		<Routes>
-			<Route path="" element={<Header />}>
-				<Route path="" element={<MainPage />}></Route>
-				<Route path="*" element={<NotFound />}></Route>
-			</Route>
+			<Route path="" element={<MainPage />}></Route>
+			<Route path="*" element={<NotFound />}></Route>
 		</Routes>
 	);
 }
