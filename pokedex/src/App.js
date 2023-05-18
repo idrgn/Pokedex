3ce30288c9eee412dea17d7434843cfd42bc8679
idrgn <37042855+idrgn@users.noreply.github.com>
2023-05-18 08:55:31 +0200

    Añadidos contenedores de página

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -7,16 +7,22 @@ import { MoveList } from "./screens/move_list/MoveList";
 import { NotFound } from "./screens/not_found/NotFound";
 import { PokemonList } from "./screens/pokemon_list/PokemonList";
 
+import "./App.css";
+
 function App() {
 	return (
-		<Routes>
-			<Route path="" element={<MainPage />}></Route>
-			<Route path="/pokemons" element={<PokemonList />}></Route>
-			<Route path="/items" element={<ItemList />}></Route>
-			<Route path="/moves" element={<MoveList />}></Route>
-			<Route path="/account" element={<Account />}></Route>
-			<Route path="*" element={<NotFound />}></Route>
-		</Routes>
+		<div className="main-container">
+			<div className="page-container">
+				<Routes>
+					<Route path="" element={<MainPage />}></Route>
+					<Route path="/pokemons" element={<PokemonList />}></Route>
+					<Route path="/items" element={<ItemList />}></Route>
+					<Route path="/moves" element={<MoveList />}></Route>
+					<Route path="/account" element={<Account />}></Route>
+					<Route path="*" element={<NotFound />}></Route>
+				</Routes>
+			</div>
+		</div>
 	);
 }
 
