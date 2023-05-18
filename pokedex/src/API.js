497b875ab9246bd91a9ccfaf6fbc6cc9355d8473
idrgn <37042855+idrgn@users.noreply.github.com>
2023-05-18 09:01:21 +0200

    Añadidas APIs

---
+++ pokedex/src/API.js
@@ -0,0 +1,5 @@
+const api_local = "http://localhost:8000/";
+const api_remota = "http://api.pokedex.lekiam.net/";
+
+export const api_pokemon = "https://pokeapi.co/api/v2/";
+export const api_privada = api_remota;
