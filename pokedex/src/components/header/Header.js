a66f2487baa7ce0453f3eb87f5b1126386e67f41
idrgn <37042855+idrgn@users.noreply.github.com>
2023-05-18 11:33:38 +0200

    Añadido menú desplegable para móvil

--- pokedex/src/components/header/Header.js
+++ pokedex/src/components/header/Header.js
@@ -1,7 +1,10 @@
 import "./Header.css";
 
+import MenuIcon from "@mui/icons-material/Menu";
+
 import { CatchingPokemonTwoTone } from "@mui/icons-material";
-import { AppBar, Button, IconButton, Stack, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Button, IconButton, Menu, MenuItem, Stack, Toolbar, Typography } from "@mui/material";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const pages = [
@@ -13,16 +16,27 @@ const pages = [
 ];
 
 export const Header = () => {
+	const [anchorElement, setAnchorElement] = useState(null);
+
+	const handleOpenMenu = (event) => {
+		setAnchorElement(event.currentTarget);
+	};
+
+	const handleCloseMenu = () => {
+		setAnchorElement(null);
+	};
+
 	return (
 		<AppBar position="static">
-			<Toolbar className="header">
+			<Toolbar className="header" justify="space-between">
 				<IconButton size="large" edge="start" color="inherit" aria-label="Logo">
 					<CatchingPokemonTwoTone />
 				</IconButton>
 				<Typography variant="h6" component={"div"} sx={{ flexGrow: 1 }}>
 					Pokedex
 				</Typography>
-				<Stack direction="row" spacing={2}>
+
+				<Stack direction="row" spacing={2} sx={{ display: { xs: "none", md: "flex" } }}>
 					{pages.map((page) => (
 						<NavLink to={page.url} style={{ textDecoration: "inherit", color: "inherit" }}>
 							<Button key={page.name} sx={{ my: 2, color: "white", display: "block" }}>
@@ -31,6 +45,38 @@ export const Header = () => {
 						</NavLink>
 					))}
 				</Stack>
+
+				<Box sx={{ display: { xs: "flex", md: "none" } }}>
+					<IconButton size="large" aria-label="Menú desplegable" aria-controls="menu-appbar" aria-haspopup="true" onClick={handleOpenMenu} color="inherit">
+						<MenuIcon />
+					</IconButton>
+					<Menu
+						id="menu-appbar"
+						anchorEl={anchorElement}
+						anchorOrigin={{
+							vertical: "bottom",
+							horizontal: "left",
+						}}
+						keepMounted
+						transformOrigin={{
+							vertical: "top",
+							horizontal: "left",
+						}}
+						open={Boolean(anchorElement)}
+						onClose={handleCloseMenu}
+						sx={{
+							display: { xs: "block", md: "none" },
+						}}
+					>
+						{pages.map((page) => (
+							<NavLink to={page.url} style={{ textDecoration: "inherit", color: "inherit" }}>
+								<MenuItem key={page.name} onClick={handleCloseMenu}>
+									<Typography textAlign="center">{page.name}</Typography>
+								</MenuItem>
+							</NavLink>
+						))}
+					</Menu>
+				</Box>
 			</Toolbar>
 		</AppBar>
 	);
