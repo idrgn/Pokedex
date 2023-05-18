3e45253af2d9d7e0bcd0c061ac589aeef613e67c
idrgn <37042855+idrgn@users.noreply.github.com>
2023-05-18 11:58:51 +0200

    Mejoras en PokemonCard, ajustado número de items

--- pokedex/src/components/pokemon_card/PokemonCard.js
+++ pokedex/src/components/pokemon_card/PokemonCard.js
@@ -1,14 +1,33 @@
 import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import { capitalizeFirstLetter } from "../../Helper";
 
 export const PokemonCard = (props) => {
 	return (
 		<Card>
 			<CardContent>
-				<Typography variant="h5" component="div">
-					{props.pokemon.name}
+				<Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+					Nº {props.pokemon.id}
 				</Typography>
-				<CardMedia component="img" height="150" width="150" image={props.pokemon.sprites.front_default} alt={props.pokemon.name} sx={{ padding: "1em 1em 0 1em", objectFit: "contain" }} />
+				<Typography variant="h5" component="div">
+					{capitalizeFirstLetter(props.pokemon.name)}
+				</Typography>
+				<CardMedia component="img" height="75" width="75" image={getBestSprite(props.pokemon.sprites)} alt={props.pokemon.name} sx={{ padding: "1em 1em 0 1em", objectFit: "contain" }} />
 			</CardContent>
 		</Card>
 	);
+};
+
+const getBestSprite = (sprites) => {
+	if (sprites.hasOwnProperty("versions")) {
+		for (let generationName in sprites.versions) {
+			const generation = sprites.versions[generationName];
+			for (let gameName in generation) {
+				const game = generation[gameName];
+				if (game.hasOwnProperty("animated")) {
+					return game.animated.front_default;
+				}
+			}
+		}
+	}
+	return sprites.front_default;
 };
--- pokedex/src/screens/pokemons/Pokemons.js
+++ pokedex/src/screens/pokemons/Pokemons.js
@@ -4,6 +4,7 @@ import { api_pokemon } from "../../API";
 import { CardGrid, GridTypes } from "../../components/card_grid/CardGrid";
 
 export const Pokemons = () => {
+	const amountPerPage = 20;
 	const [currentIndex, setCurrentIndex] = useState(0);
 	const [pokemonData, setPokemonData] = useState(null);
 	const [processedPokemonList, setProcessedPokemonList] = useState([]);
@@ -17,13 +18,13 @@ export const Pokemons = () => {
 	useEffect(() => {
 		if (pokemonData === null) return;
 
-		for (let entry of pokemonData.results.slice(currentIndex, currentIndex + 10)) {
+		for (let entry of pokemonData.results.slice(currentIndex, currentIndex + amountPerPage)) {
 			axios.get(entry.url).then((response) => {
 				setProcessedPokemonList((prevList) => prevList.concat(response.data));
 			});
 		}
 
-		// setCurrentIndex((prevIndex) => prevIndex + 10);
+		// setCurrentIndex((prevIndex) => prevIndex + amountPerPage);
 	}, [currentIndex, pokemonData]);
 
 	return (
