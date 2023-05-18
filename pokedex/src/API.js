58f2b7cf6c50d7e40be125606283a0e8e27f6f4d
idrgn <37042855+idrgn@users.noreply.github.com>
2023-05-18 09:02:35 +0200

    Comentarios en el archivo APIs

--- pokedex/src/API.js
+++ pokedex/src/API.js
@@ -1,5 +1,9 @@
+/* eslint-disable no-unused-vars */
+// APIs creadas para el proyecto
 const api_local = "http://localhost:8000/";
 const api_remota = "http://api.pokedex.lekiam.net/";
 
-export const api_pokemon = "https://pokeapi.co/api/v2/";
 export const api_privada = api_remota;
+
+// APIs públicas
+export const api_pokemon = "https://pokeapi.co/api/v2/";
