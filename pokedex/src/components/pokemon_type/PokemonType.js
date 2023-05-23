f12e0180c2d9b4096d837a821645915eac66ca11
idrgn <37042855+idrgn@users.noreply.github.com>
2023-05-23 13:34:53 +0200

    Corrección de error al cargar tipos

--- pokedex/src/components/pokemon_type/PokemonType.js
+++ pokedex/src/components/pokemon_type/PokemonType.js
@@ -1,6 +1,6 @@
 import "./PokemonType.css";
 
 export const PokemonType = (props) => {
-	if (props.size === null) return <span className={`span type ${props.type}`} />;
+	if (!props.hasOwnProperty("size")) return <span className={`span type ${props.type}`} />;
 	if (props.size === "big") return <span className={`span type ${props.type} big-type`} />;
 };
