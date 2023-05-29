9b645b2279950734fa3be5fc8c918ea77c026bf2
idrgn <37042855+idrgn@users.noreply.github.com>
2023-05-29 08:57:03 +0200

    Creado componente itemGrid

--- pokedex/src/components/card_grid/CardGrid.js
+++ pokedex/src/components/card_grid/CardGrid.js
@@ -1,5 +1,6 @@
 import { Grid } from "@mui/material";
 import { useState } from "react";
+import { ItemCard } from "../item_card/ItemCard";
 import { PokemonCard } from "../pokemon_card/PokemonCard";
 
 import "./CardGrid.css";
@@ -32,6 +33,29 @@ export const PokemonGrid = (props) => {
 			{props.pokemonData.map((p) => (
 				<Grid item onClick={() => onSelect(p)} className="pokemon-grid-container">
 					<PokemonCard pokemon={p} selected={p.id === selected} animated={props.animated}></PokemonCard>
+				</Grid>
+			))}
+		</Grid>
+	);
+};
+
+export const ItemGrid = (props) => {
+	const [selected, setSelected] = useState(null);
+
+	const onSelect = (i) => {
+		if (selected !== i.id) {
+			setSelected(i.id);
+			if (props.hasOwnProperty("onSelectionChanged")) {
+				props.onSelectionChanged(i);
+			}
+		}
+	};
+
+	return (
+		<Grid container rowSpacing={{ xs: 1, sm: 2, md: 3 }} columnSpacing={{ xs: 1, sm: 2, md: 3 }} sx={{ display: "flex", justifyContent: "center" }}>
+			{props.itemData.map((i) => (
+				<Grid item onClick={() => onSelect(i)} className="item-grid-container">
+					<ItemCard item={i} selected={i.id === selected}></ItemCard>
 				</Grid>
 			))}
 		</Grid>
