f4562904f2cb8f88a522993f994fb336d407d1b4
idrgn <37042855+idrgn@users.noreply.github.com>
2023-05-29 19:42:55 +0200

    Ajustes de estilos y tamaños en PokemonType

--- pokedex/src/components/pokemon_type/PokemonType.js
+++ pokedex/src/components/pokemon_type/PokemonType.js
@@ -1,6 +1,7 @@
 import "./PokemonType.css";
 
 export const PokemonType = (props) => {
-	if (!props.hasOwnProperty("size")) return <span className={`span type ${props.type}`} />;
-	if (props.size === "big") return <span className={`span type ${props.type} big-type`} />;
+	if (!props.hasOwnProperty("size")) return <span className={`span type ${props.type} med-size`} />;
+	if (props.size === "big") return <span className={`span type ${props.type} big-size`} />;
+	if (props.size === "med") return <span className={`span type ${props.type} med-size`} />;
 };
