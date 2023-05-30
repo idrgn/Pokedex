8eee68a11fcd58006587da67cfb1916b86e6779b
idrgn <37042855+idrgn@users.noreply.github.com>
2023-05-30 11:25:30 +0200

    Añadido componente vacío para movimientos

---
+++ pokedex/src/components/move_card/MoveCard.js
@@ -0,0 +1,16 @@
+import { Card, CardContent, Typography } from "@mui/material";
+import { getLightColor } from "../../Helper";
+
+import "./MoveCard.css";
+
+export const MoveCard = (props) => {
+	return (
+		<Card sx={{ borderRadius: "10px" }} variant="outlined" className="move-card" style={{ backgroundColor: getLightColor("red") }}>
+			<CardContent>
+				<Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+					Nº {props.move.id}
+				</Typography>
+			</CardContent>
+		</Card>
+	);
+};
