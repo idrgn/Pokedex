2179f014bd70691dfa346b1fc4317ac75d3f4c7e
idrgn <37042855+idrgn@users.noreply.github.com>
2023-05-30 15:55:29 +0200

    Nuevos datos y CSS de Moves

--- pokedex/src/components/move/move_card/MoveCard.js
+++ pokedex/src/components/move/move_card/MoveCard.js
@@ -1,16 +1,19 @@
 import { Card, CardContent, Typography } from "@mui/material";
-import { getLightColor } from "../../../Helper";
+import { getFlavorText, getLightColor, getName } from "../../../Helper";
 
 import "./MoveCard.css";
+import { MoveType } from "../move_type/MoveType";
 
 export const MoveCard = (props) => {
 	return (
 		<Card sx={{ borderRadius: "10px" }} variant="outlined" className="move-card" style={{ backgroundColor: getLightColor("red") }}>
+			<CardContent sx={{ display: "flex", justifyContent: "space-between" }}>
+				<Typography>{getName(props.move.names)}</Typography>
+				<MoveType type={props.move.damage_class.name}></MoveType>
+			</CardContent>
+
 			<CardContent>
-				<Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-					Nº {props.move.id}
-				</Typography>
-				<Typography sx={{ fontSize: 14 }}>Nº {props.move.name}</Typography>
+				<Typography sx={{ fontSize: 14 }}>{getFlavorText(props.move.flavor_text_entries)}</Typography>
 			</CardContent>
 		</Card>
 	);
