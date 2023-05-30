408170885424ec6887caf117390478d8a31c0c08
idrgn <37042855+idrgn@users.noreply.github.com>
2023-05-30 17:42:58 +0200

    Añadido tipo y estadísticas de movimiento

--- pokedex/src/components/move/move_card/MoveCard.js
+++ pokedex/src/components/move/move_card/MoveCard.js
@@ -1,19 +1,30 @@
 import { Card, CardContent, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import { getFlavorText, getLightColor, getName } from "../../../Helper";
-
-import "./MoveCard.css";
+import { Type } from "../../type/Type";
 import { MoveType } from "../move_type/MoveType";
+import "./MoveCard.css";
 
 export const MoveCard = (props) => {
 	return (
-		<Card sx={{ borderRadius: "10px" }} variant="outlined" className="move-card" style={{ backgroundColor: getLightColor("red") }}>
+		<Card sx={{ borderRadius: "10px" }} variant="outlined" className="move-card" style={{ backgroundColor: getLightColor("salmon") }}>
 			<CardContent sx={{ display: "flex", justifyContent: "space-between" }}>
-				<Typography>{getName(props.move.names)}</Typography>
-				<MoveType type={props.move.damage_class.name}></MoveType>
+				<Box sx={{ display: "flex" }}>
+					<MoveType type={props.move.damage_class.name}></MoveType>
+					<Typography sx={{ paddingLeft: "5px" }}>{getName(props.move.names)}</Typography>
+				</Box>
+
+				<Type type={props.move.type.name} size="sma"></Type>
 			</CardContent>
 
 			<CardContent>
 				<Typography sx={{ fontSize: 14 }}>{getFlavorText(props.move.flavor_text_entries)}</Typography>
+			</CardContent>
+
+			<CardContent sx={{ display: "flex", justifyContent: "space-between" }}>
+				<Typography>{`Daño: ${props.move.power ? props.move.power : "-"}`}</Typography>
+				<Typography>{`Usos: ${props.move.pp ? props.move.pp : "-"}`}</Typography>
+				<Typography>{`Precisión: ${props.move.chance ? props.move.chance : "-"}`}</Typography>
 			</CardContent>
 		</Card>
 	);
--- pokedex/src/components/type/Type.js
+++ pokedex/src/components/type/Type.js
@@ -4,4 +4,5 @@ export const Type = (props) => {
 	if (!props.hasOwnProperty("size")) return <span className={`span type ${props.type} med-size`} />;
 	if (props.size === "big") return <span className={`span type ${props.type} big-size`} />;
 	if (props.size === "med") return <span className={`span type ${props.type} med-size`} />;
+	if (props.size === "sma") return <span className={`span type ${props.type} sma-size`} />;
 };
