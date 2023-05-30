800404476f19f119be917ab728dfa21db6ca6638
idrgn <37042855+idrgn@users.noreply.github.com>
2023-05-30 22:47:22 +0200

    Ajusted en itemDetail

--- pokedex/src/components/item/item_detail/ItemDetail.js
+++ pokedex/src/components/item/item_detail/ItemDetail.js
@@ -1,5 +1,6 @@
 import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 import { getFlavorText, getLightColor, getName } from "../../../Helper";
+import { Box } from "@mui/system";
 
 import "./ItemDetail.css";
 
@@ -16,25 +17,20 @@ export const ItemDetail = (props) => {
 
 	return (
 		<Card sx={{ backgroundColor: getLightColor("lightgreen") }} className="item-detail-container">
-			<CardContent className="item-detail-box">
-				<CardMedia sx={{ height: "100px", width: "100%", objectFit: "contain" }} component="img" image={props.item.sprites.default} alt={getName(props.item.names)} />
-			</CardContent>
-
-			<CardContent className="item-detail-box">
-				<Typography sx={{ textAlign: "center" }} variant="h4" component="div">
-					{getName(props.item.names)}
-				</Typography>
-			</CardContent>
+			<Box sx={{ display: "flex" }}>
+				<CardContent className="item-detail-box">
+					<CardMedia sx={{ height: "100px", width: "100%", objectFit: "contain" }} component="img" image={props.item.sprites.default} alt={getName(props.item.names)} />
+				</CardContent>
+				<CardContent className="item-detail-box">
+					<Typography sx={{ textAlign: "center" }} variant="h4" component="div">
+						{getName(props.item.names)}
+					</Typography>
+				</CardContent>
+			</Box>
 
 			<CardContent className="item-detail-box">
 				<Typography sx={{ textAlign: "center", padding: "20px" }} variant="body2" component="div">
 					{getFlavorText(props.item.flavor_text_entries)}
-				</Typography>
-			</CardContent>
-
-			<CardContent className="item-detail-box">
-				<Typography sx={{ fontSize: 14, textAlign: "center" }} color="text.secondary" gutterBottom>
-					Nº {props.item.id}
 				</Typography>
 			</CardContent>
 		</Card>
