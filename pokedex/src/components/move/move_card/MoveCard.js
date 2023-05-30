e1e570eb18f40431af18eb2beb3efa8f3f583778
idrgn <37042855+idrgn@users.noreply.github.com>
2023-05-30 17:51:28 +0200

    Añadidos estilos de atributos de movimiento

--- pokedex/src/components/move/move_card/MoveCard.js
+++ pokedex/src/components/move/move_card/MoveCard.js
@@ -3,6 +3,7 @@ import { Box } from "@mui/system";
 import { getFlavorText, getLightColor, getName } from "../../../Helper";
 import { Type } from "../../type/Type";
 import { MoveType } from "../move_type/MoveType";
+
 import "./MoveCard.css";
 
 export const MoveCard = (props) => {
@@ -18,13 +19,24 @@ export const MoveCard = (props) => {
 			</CardContent>
 
 			<CardContent>
-				<Typography sx={{ fontSize: 14 }}>{getFlavorText(props.move.flavor_text_entries)}</Typography>
+				<Typography sx={{ fontSize: 14, textAlign: "center" }}>{getFlavorText(props.move.flavor_text_entries)}</Typography>
 			</CardContent>
 
-			<CardContent sx={{ display: "flex", justifyContent: "space-between" }}>
-				<Typography>{`Daño: ${props.move.power ? props.move.power : "-"}`}</Typography>
-				<Typography>{`Usos: ${props.move.pp ? props.move.pp : "-"}`}</Typography>
-				<Typography>{`Precisión: ${props.move.chance ? props.move.chance : "-"}`}</Typography>
+			<CardContent sx={{ display: "flex", justifyContent: "space-around" }}>
+				<Box className="single-move-attribute-container">
+					<Typography className="attribute-single">{props.move.power ? props.move.power : "—"}</Typography>
+					<Typography variant="overline">Daño</Typography>
+				</Box>
+
+				<Box className="single-move-attribute-container">
+					<Typography className="attribute-single">{props.move.pp ? props.move.pp : "—"}</Typography>
+					<Typography variant="overline">Usos</Typography>
+				</Box>
+
+				<Box className="single-move-attribute-container">
+					<Typography className="attribute-single">{props.move.chance ? props.move.chance : "—"}</Typography>
+					<Typography variant="overline">Precisión</Typography>
+				</Box>
 			</CardContent>
 		</Card>
 	);
