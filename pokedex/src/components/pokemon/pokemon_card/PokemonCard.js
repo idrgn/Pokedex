30135bf5f5944b0ec05cbeae9a1ea3e439cc352c
idrgn <37042855+idrgn@users.noreply.github.com>
2023-05-30 17:33:35 +0200

    Renombrado pokemonType a Type

--- pokedex/src/components/pokemon/pokemon_card/PokemonCard.js
+++ pokedex/src/components/pokemon/pokemon_card/PokemonCard.js
@@ -1,6 +1,6 @@
 import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 import { capitalizeFirstLetter, getBestSprite } from "../../../Helper";
-import { PokemonType } from "../../type/Type";
+import { Type } from "../../type/Type";
 
 import "./PokemonCard.css";
 
@@ -18,7 +18,7 @@ export const PokemonCard = (props) => {
 			<CardMedia component="img" height="75" width="75" image={props.animated ? getBestSprite(props.pokemon.sprites) : props.pokemon.sprites.front_default} alt={props.pokemon.name} sx={{ paddingTop: "0.25em", objectFit: "contain" }} />
 			<CardContent sx={{ display: "flex", justifyContent: "space-around" }}>
 				{props.pokemon.types.map((t) => (
-					<PokemonType type={t.type.name}></PokemonType>
+					<Type type={t.type.name}></Type>
 				))}
 			</CardContent>
 		</Card>
--- pokedex/src/components/type/Type.js
+++ pokedex/src/components/type/Type.js
@@ -1,6 +1,6 @@
 import "./Type.css";
 
-export const PokemonType = (props) => {
+export const Type = (props) => {
 	if (!props.hasOwnProperty("size")) return <span className={`span type ${props.type} med-size`} />;
 	if (props.size === "big") return <span className={`span type ${props.type} big-size`} />;
 	if (props.size === "med") return <span className={`span type ${props.type} med-size`} />;
