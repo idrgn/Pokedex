10503b41e21958b40268ffadf69069fe601ed17b
idrgn <37042855+idrgn@users.noreply.github.com>
2023-06-05 03:51:42 +0200

    Creada página principal con imagen de fondo

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,9 +1,8 @@
-import { Route, Routes } from "react-router-dom";
-
-import { Items } from "./screens/items/Items";
-// import { MainPage } from "./screens/main_page/MainPage";
 import { ThemeProvider } from "@emotion/react";
 import { createTheme } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import { Items } from "./screens/items/Items";
+import { MainPage } from "./screens/main_page/MainPage";
 import { Moves } from "./screens/moves/Moves";
 import { NotFound } from "./screens/not_found/NotFound";
 import { Pokemons } from "./screens/pokemons/Pokemons";
@@ -24,7 +23,7 @@ function App() {
 			<div className="main-container">
 				<div className="page-container">
 					<Routes>
-						<Route path="" element={<Pokemons />}></Route>
+						<Route path="" element={<MainPage />}></Route>
 						<Route path="/pokemons" element={<Pokemons />}></Route>
 						<Route path="/items" element={<Items />}></Route>
 						<Route path="/moves" element={<Moves />}></Route>
--- pokedex/src/screens/main_page/MainPage.js
+++ pokedex/src/screens/main_page/MainPage.js
@@ -1,3 +1,5 @@
+import "./MainPage.css";
+
 /**
  * Pantalla principal
  * @param {*} props
@@ -6,7 +8,12 @@
 export const MainPage = (props) => {
 	return (
 		<div>
-			<h1>Página principal</h1>
+			<div
+				className="main-page-container"
+				style={{
+					backgroundImage: `url(${process.env.PUBLIC_URL + "/background.jpg"})`,
+				}}
+			></div>
 		</div>
 	);
 };
